40b5f3e1d3bbe8d5666327da897df3f08de3553f
TaeHoon Kim <kth20970@naver.com>
2018-03-28 20:05:43 +0900

    3-28-taehoon

---
+++ ThisIsDisaster/Assets/Scripts/CameraControl/Chasing.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chasing : MonoBehaviour {
+    public GameObject Player;
+    public GameObject Target;
+    public float CameraZ = -10; // 카메라 높이 -10으로 고정
+	// Update와 다르게 프레임 상관없이 업데이트
+	void FixedUpdate () {
+        Vector3 TargetPos = new Vector3(Target.transform.position.x, Target.transform.position.y, CameraZ);
+        transform.position = Vector3.Lerp(transform.position, TargetPos, Time.deltaTime * 2f);
+	}
+}
---
+++ ThisIsDisaster/Assets/Scripts/PlayerControl/PlayerMoveController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveController : MonoBehaviour {
+    public float moveSpeed = 1f;
+
+    void Update() {
+        Vector3 currentPos = transform.position;
+        if (Input.GetKey(KeyCode.W))
+        {
+            MoveUp(ref currentPos);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            MoveDown(ref currentPos);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            MoveLeft(ref currentPos);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            MoveRight(ref currentPos);
+        }
+    }
+    //deltaTime : 프레임에 렉이 걸린만큼 값이 커져 프레임렉을 보정
+    void MoveUp(ref Vector3 pos)
+    {
+        transform.Translate(0,moveSpeed * Time.deltaTime, 0);
+    }
+
+    void MoveDown(ref Vector3 pos)
+    {
+
+        transform.Translate(0, -moveSpeed * Time.deltaTime, 0);
+    }
+
+    void MoveLeft(ref Vector3 pos)
+    {
+
+        transform.Translate(-moveSpeed * Time.deltaTime, 0, 0);
+    }
+
+    void MoveRight(ref Vector3 pos)
+    {
+
+        transform.Translate(moveSpeed * Time.deltaTime, 0, 0);
+    }
+}
