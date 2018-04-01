089da8ab8ec4a6fbe13e43d0cd1c23de5af17918
Youngmin Kim <rladudals02@gmail.com>
2018-04-01 18:25:27 +0900

    Roamer
    1) 기본 움직이는 적 추가
    2) 충돌 테스트 - 동작 x, 수정 필요
    - collider 추가
    - rigid body 추가

---
+++ ThisIsDisaster/Assets/Scripts/NPCControl/NPCPatrol.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCPatrol : MonoBehaviour {
+
+	public float speed;
+	private float waitTime;
+	public float startWaitTime;
+
+	public Transform[] moveSpots;
+	private int randomSpot;
+
+
+	void Start () {
+		waitTime = startWaitTime;
+		randomSpot = Random.Range (0, moveSpots.Length);
+	}
+
+
+	void Update () {
+		transform.position = Vector2.MoveTowards (transform.position, moveSpots [randomSpot].position, speed * Time.deltaTime);
+
+		if (Vector2.Distance (transform.position, moveSpots [randomSpot].position) < 0.2f) {
+			if (waitTime <= 0) {
+				randomSpot = Random.Range (0, moveSpots.Length);
+				waitTime = startWaitTime;
+			} else {
+				waitTime -= Time.deltaTime;
+			}
+		}
+	}
+}
