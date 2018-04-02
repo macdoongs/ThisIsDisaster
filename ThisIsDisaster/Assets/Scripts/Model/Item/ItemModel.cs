58e0c5d74cab2d267b1b22f0dafa8e7a865c710f
Hyungjun <nighthawk189@gmail.com>
2018-04-02 20:38:52 +0900

    Item 관련 구조 제작중 : Item Model -> 아이템에 대한 코드상의 오브젝트입니다, 유니티 게임 상의 오브젝트가 아닙니다. Item Manager -> 아이템을 관리하기 위한 관리자 클래스입니다. Item TypeInfo -> 아이템의 이름, 수량, 타입 등을 저장하고 있는 데이터입니다. Unit Model -> 플레이어, NPC등의 Actor들을 위한 코드상의 모델입니다

---
+++ ThisIsDisaster/Assets/Scripts/Model/Item/ItemModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class ItemModel {
+    //Who is Owner
+
+    //field will be initialized
+    //Think which will be needed
+
+    public ItemTypeInfo metaInfo;
+    public long instanceId;
+
+    public UnitModel Owner = null;
+
+    public virtual void OnItemAqquired(UnitModel target) {
+        //do smth
+        Owner = target;
+    }
+
+    public virtual void OnItemRemoved(UnitModel prevOwner) {
+        //do smth
+        Owner = null;
+    }
+}
+
+public class DummyItem : ItemModel {
+    
+}
---
+++ ThisIsDisaster/Assets/Scripts/Model/Item/ItemTypeInfo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/// <summary>
+/// 아이템의 종류 -> 장비 혹은 소모품
+/// </summary>
+public enum ItemType
+{
+    /// <summary>
+    /// 장비 - 단일 존재 개체
+    /// </summary>
+    EQUIPMENT,
+
+    /// <summary>
+    /// 소모품 - 다수 소지 가능
+    /// </summary>
+    EXPENDABLES
+}
+
+public class ItemTypeInfo
+{
+    /// <summary>
+    /// 아이템 이름
+    /// </summary>
+    public string Name;
+
+    /// <summary>
+    /// 소지가능한 최대 개수 : 장비의 경우 1개, 소모품의 경우 _defaultMaxCount를 반환
+    /// </summary>
+    public int MaxCount
+    {
+        get { return itemType == ItemType.EQUIPMENT ? 1 : _defaultMaxCount; }
+    }
+
+    private int _defaultMaxCount = 0;
+
+    /// <summary>
+    /// 아이템의 타입 - 장비, 소모품
+    /// </summary>
+    public ItemType itemType = ItemType.EQUIPMENT;
+
+    //smth else info
+}
