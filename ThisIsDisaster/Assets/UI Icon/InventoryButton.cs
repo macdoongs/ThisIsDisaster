5b4016188ca38e9e3ac72f7449068d32c870d4a3
박성주 <bsj5899@naver.com>
2018-04-02 20:03:36 +0900

    meta

---
+++ ThisIsDisaster/Assets/UI Icon/InventoryButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class InventoryButton : MonoBehaviour {
+
+    public GameObject InventoryInside;
+        
+    public void InventoryInsideActive()
+    {
+        InventoryInside.SetActive(true);
+    }
+
+
+}
