f641ed35ec5a1025c9f392a7d1ad984c03b47534
ParkSeongJoo <bsj5899@naver.com>
2018-04-04 18:36:00 +0900

    아이템바 수정. 액션버튼 추가

---
+++ ThisIsDisaster/Assets/UI/UI Scripts/ItmeFocused.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItmeFocused : MonoBehaviour {
+
+    public Image border;
+
+    public void itemFocused()
+    {
+        Color newColor = Color.blue;
+        Debug.Log("Button Pressed");
+        border.color = newColor;
+
+    }
+}
