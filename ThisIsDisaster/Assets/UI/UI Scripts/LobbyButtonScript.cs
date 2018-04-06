3dc97a8dc97c9f804537ca0fafab6d0a411abbec
박성주 <bsj5899@naver.com>
2018-04-06 16:10:17 +0900

    UI  버튼 포커스

---
+++ ThisIsDisaster/Assets/UI/UI Scripts/LobbyButtonScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyButtonScript : MonoBehaviour {
+
+    public Text focusedText;
+    public GameObject focusedUnderLine;
+
+    public Text anotherText1;
+    public GameObject anotherUnderLIne1;
+
+    public Text anotherText2;
+    public GameObject anotherUnderLIne2;
+    
+
+    public void OnClick()
+    {
+        focusedText.color = Color.yellow;
+        focusedUnderLine.SetActive(true);
+
+        anotherText1.color = Color.white;
+        anotherUnderLIne1.SetActive(false);
+        anotherText2.color = Color.white;
+        anotherUnderLIne2.SetActive(false);
+
+    }
+    
+}
