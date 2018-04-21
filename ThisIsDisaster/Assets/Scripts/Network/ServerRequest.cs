7a71f728b9c7b45fe17b5416a9015aae11cd8f32
Youngmin Kim <rladudals02@gmail.com>
2018-04-22 01:28:13 +0900

    Server Interworking form

---
+++ ThisIsDisaster/Assets/Scripts/Network/ServerRequest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+
+public class ServerRequest : MonoBehaviour{
+	void Start() {
+		StartCoroutine(GetText());
+	}
+
+	IEnumerator GetText() {
+		UnityWebRequest www = UnityWebRequest.Get("http://localhost:9000/user");
+		yield return www.Send();
+
+		if(www.isNetworkError) {
+			Debug.Log(www.error);
+		}
+		else {
+			// Show results as text
+			Debug.Log(www.downloadHandler.text);
+
+			UserResponse userResponse;
+
+			string json = www.downloadHandler.text;
+
+			userResponse = JsonUtility.FromJson<UserResponse>(json);
+
+			Debug.Log (userResponse.result_data);
+		}
+	}
+}
