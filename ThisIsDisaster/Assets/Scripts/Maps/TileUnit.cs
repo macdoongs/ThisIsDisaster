7cbaba05723b22105e5b8bcb2cbb0c59cc96d894
Hyungjun <nighthawk189@gmail.com>
2018-05-02 18:32:22 +0900

    reverse missing

---
+++ ThisIsDisaster/Assets/Scripts/Maps/TileUnit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TileType {
+    WATER,
+    GROUND_WALKABLE,
+    GROUND_UNWALKABLE
+}
+
+public class TileUnit : MonoBehaviour {
+    public TileType type;
+    public TempTileModel _model;
+    public SpriteRenderer spriteRenderer;
+
+#if PROTOTYPE
+    public bool isNearWater = false;
+#endif
+
+    public void SetModel(TempTileModel model){
+        _model = model;
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+}
