3687c7b003ae7106bd519692730046d64a416211
주형준 <nighthawk189@gmail.com>
2018-05-03 10:17:18 +0900

    Controller & Behaviour Initialized

---
+++ ThisIsDisaster/Assets/Scripts/Model/Behaviour/Controller/PlayerBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : UnitBehaviourBase
+{
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public virtual bool IsLocal() {
+        return true;
+    }
+    
+}
---
+++ ThisIsDisaster/Assets/Scripts/Model/Behaviour/Controller/UnitControllerBase.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitControllerBase : MonoBehaviour
+{
+    public UnitBehaviourBase behaviour = null;
+    private ItemManager _itemManager = null;
+
+    private void Awake()
+    {
+        _itemManager = ItemManager.Manager;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public Vector3 GetPosition() {
+        return transform.position;
+    }
+
+    public void SetPosition(Vector3 position)
+    {
+        transform.position = position;
+    }
+
+    public float GetDirection() {
+        return transform.localScale.x;
+    }
+
+
+}
