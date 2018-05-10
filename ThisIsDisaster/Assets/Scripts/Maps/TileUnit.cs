0f518014d39489260e9ac2867ffb07ab7cf96d8c
주형준 <nighthawk189@gmail.com>
2018-05-10 22:51:15 +0900

    타일 좌표 습득 구현: 테스트용 코드-> PlayerCurrentTile (디버그용 코드)

--- ThisIsDisaster/Assets/Scripts/Maps/TileUnit.cs
+++ ThisIsDisaster/Assets/Scripts/Maps/TileUnit.cs
@@ -9,14 +9,19 @@ public enum TileType {
 }
 
 public class TileUnit : MonoBehaviour {
+    public const float _DEF_HEIGHT = -0.5f + 0.135f;
     public TileType type;
     public TempTileModel _model;
     public SpriteRenderer spriteRenderer;
+    public new Collider2D collider;
 
     public int HeightLevel = 0;
     public Vector3 originalPosition;
 
     public bool isNearWater = false;
+
+    public int x = 0, y = 0;
+
     public void SetModel(TempTileModel model){
         _model = model;
         //spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
@@ -25,10 +30,18 @@ public class TileUnit : MonoBehaviour {
     public void SetPosition(Vector3 pos) {
         originalPosition = pos;
         transform.localPosition = pos;
+        Vector3 colPos = collider.transform.position;
+        colPos.z = 0f;
+        collider.transform.position = colPos;
+    }
+
+    public void SetCoord(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
     public void SetHeight(int heightLevel) {
         HeightLevel = heightLevel;
-        spriteRenderer.transform.localPosition = new Vector3(0, 0.25f * heightLevel, 0f);
+        spriteRenderer.transform.localPosition = new Vector3(0, 0.25f * heightLevel + _DEF_HEIGHT, 0f);
     }
 }
---
+++ ThisIsDisaster/Assets/Scripts/PlayerControl/PlayerCurrentTile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 디버그용
+/// </summary>
+public class PlayerCurrentTile : MonoBehaviour {
+    public Text debugText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (debugText) {
+            var tile = RandomMapGenerator.Instance.GetTile(transform.position);
+            if (tile != null)
+            {
+                debugText.text = tile.x + " " + tile.y;
+            }
+            else {
+                debugText.text = "ERROR";
+            }
+        }
+	}
+}
