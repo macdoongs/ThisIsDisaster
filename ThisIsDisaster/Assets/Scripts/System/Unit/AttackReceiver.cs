7177b644e64725a91553b7c9196387adc89140a9
주형준 <nighthawk189@gmail.com>
2018-05-16 11:20:38 +0900

    attack reciever

---
+++ ThisIsDisaster/Assets/Scripts/System/Unit/AttackReceiver.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackReceiver : MonoBehaviour
+{
+    public Collider2D _reciveCollider;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (_reciveCollider == null) {
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
